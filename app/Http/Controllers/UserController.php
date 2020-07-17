14311333bd9f4b8a572161e7aa28e9fb273abcfd
Elias Diek <elias@holamicasa.com>
2020-07-18 04:31:23 +0800

    google recaptcha2 on the contact page & no package issue

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -84,19 +84,25 @@ class UserController extends Controller
             ['status','=',1]
         ])->count();
 
-        if(null !== Auth::user()->pricing && 0 !== Auth::user()->pricing) {
-            $current_plan = Auth::user()->package->rows;
-            if(null !== Auth::user()->processed) {
-                $processable_rows = Auth::user()->package->rows - Auth::user()->processed;
+        try {
+            if(null !== Auth::user()->pricing && 0 !== Auth::user()->pricing) {
+                $current_plan = Auth::user()->package->rows;
+                if(null !== Auth::user()->processed) {
+                    $processable_rows = Auth::user()->package->rows - Auth::user()->processed;
+                }
+                else {
+                    $processable_rows = Auth::user()->package->rows - 0;
+                }
             }
             else {
-                $processable_rows = Auth::user()->package->rows - 0;
+                $current_plan = 0;
+                $processable_rows = 0;
             }
-        }
-        else {
+        } catch (Exception $e) {
             $current_plan = 0;
             $processable_rows = 0;
         }
+
 
         $user = User::where('id','=',Auth::user()->id)->first();
 
@@ -128,16 +134,21 @@ class UserController extends Controller
             ['status','=',1]
         ])->count();
 
-        if(null !== Auth::user()->pricing && 0 !== Auth::user()->pricing) {
-            $current_plan = Auth::user()->package->rows;
-            if(null !== Auth::user()->processed) {
-                $processable_rows = Auth::user()->package->rows - Auth::user()->processed;
+        try {
+            if(null !== Auth::user()->pricing && 0 !== Auth::user()->pricing) {
+                $current_plan = Auth::user()->package->rows;
+                if(null !== Auth::user()->processed) {
+                    $processable_rows = Auth::user()->package->rows - Auth::user()->processed;
+                }
+                else {
+                    $processable_rows = Auth::user()->package->rows - 0;
+                }
             }
             else {
-                $processable_rows = Auth::user()->package->rows - 0;
+                $current_plan = 0;
+                $processable_rows = 0;
             }
-        }
-        else {
+        } catch (Exception $e) {
             $current_plan = 0;
             $processable_rows = 0;
         }
@@ -172,16 +183,21 @@ class UserController extends Controller
             ['status','=',1]
         ])->count();
 
-        if(null !== Auth::user()->pricing && 0 !== Auth::user()->pricing) {
-            $current_plan = Auth::user()->package->rows;
-            if(null !== Auth::user()->processed) {
-                $processable_rows = Auth::user()->package->rows - Auth::user()->processed;
+        try {
+            if(null !== Auth::user()->pricing && 0 !== Auth::user()->pricing) {
+                $current_plan = Auth::user()->package->rows;
+                if(null !== Auth::user()->processed) {
+                    $processable_rows = Auth::user()->package->rows - Auth::user()->processed;
+                }
+                else {
+                    $processable_rows = Auth::user()->package->rows - 0;
+                }
             }
             else {
-                $processable_rows = Auth::user()->package->rows - 0;
+                $current_plan = 0;
+                $processable_rows = 0;
             }
-        }
-        else {
+        } catch (Exception $e) {
             $current_plan = 0;
             $processable_rows = 0;
         }
